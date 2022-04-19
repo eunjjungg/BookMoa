c3425a942cd3a7068da613dead3cabe981eda1fb
eunjung <agjjung@naver.com>
2022-04-19 12:19:51 +0900

    0418 build.gradle 수정 input_box.xml 생성 manifest 인터넷 권한 추가 begin_login 코틀린, xml 파일 생성

--- app/src/main/java/com/smu/bookmoa/Begin_Login.kt
+++ app/src/main/java/com/smu/bookmoa/Begin_Login.kt
@@ -2,10 +2,45 @@ package com.smu.bookmoa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.smu.bookmoa.databinding.ActivityBeginLoginBinding
 
-class Begin_Login : AppCompatActivity() {
+class Begin_Login : AppCompatActivity(), View.OnClickListener {
+    val binding by lazy { ActivityBeginLoginBinding.inflate(layoutInflater) }
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_begin_login)
+        setContentView(binding.root)
+        auth = Firebase.auth
+    }
+
+    override fun onClick(p0: View?) {
+        when(p0?.id) {
+            binding.btnLogIn.id -> {
+                val email = binding.etId.text.toString()
+                val password = binding.etPw.text.toString()
+                logInProcess(email, password)
+            }
+        }
+    }
+
+    fun logInProcess(email:String, password:String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d("TAG", "signInWithEmail:success")
+                    Toast.makeText(baseContext, "Sign in Success.", Toast.LENGTH_SHORT).show()
+                    val user = auth.currentUser
+                } else {
+                    Log.w("TAG", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
