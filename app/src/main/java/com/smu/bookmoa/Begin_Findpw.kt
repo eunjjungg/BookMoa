b1e12fd73b5b71cd990e8e590567808d75194649
민경 <tjalsrud0101@gmail.com>
2022-04-19 13:45:31 +0900

    회원가입, 비밀번호 찾기, 뒤로가기 구현

--- app/src/main/java/com/smu/bookmoa/Begin_Findpw.kt
+++ app/src/main/java/com/smu/bookmoa/Begin_Findpw.kt
@@ -1,11 +1,38 @@
 package com.smu.bookmoa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.smu.bookmoa.databinding.ActivityBeginFindpwBinding
 
 class Begin_Findpw : AppCompatActivity() {
+
+    val binding by lazy { ActivityBeginFindpwBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_begin_findpw)
+        setContentView(binding.root)
+
+        binding.btnBack.setOnClickListener {
+            onBackPressed()
+        }
+
+        binding.btnFindpw.setOnClickListener {
+            val email = binding.txtEmail.text.toString()
+            if(!email.isEmpty())
+                finPW(email)
+        }
+    }
+
+    private fun finPW(email: String) {
+        FirebaseAuth.getInstance().sendPasswordResetEmail(email).addOnCompleteListener {
+            if(it.isSuccessful) {
+                Toast.makeText(this, "Email sent", Toast.LENGTH_SHORT).show()
+                val intentBeginActivity = Intent(this, BeginActivity::class.java)
+                startActivity(intentBeginActivity)
+            }
+        }
     }
 }
